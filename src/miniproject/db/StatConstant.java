0dec74944521fda4227d66d4077582ebcb189242
soyoonjeong2328 <soyounjeong6276@gmail.com>
2023-10-01 11:29:58 +0900

    remove: db info

--- src/miniproject/db/StatConstant.java
+++
@@ -1,9 +0,0 @@
-package miniproject.db;
-
-public class StatConstant {
-    public static final int JmxHeapDataStat = 1001;
-    public static final int JmxClassData = 1002;
-    public static final int JmxThreadData = 1003;
-    public static final int JmxGcData = 1004;
-    public static final int ClassStat = 1005;
-}
--- src/training/db/StatConstant.java
+++
@@ -1,7 +0,0 @@
-package training.db;
-
-public class StatConstant {
-    public static final int DB_STAT = 2;
-    public static final int SESSION_STAT = 3;
-    public static final int SQL_STAT = 4;
-}
